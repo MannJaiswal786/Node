ce35e9b7868470b4f660fb66d0f45784823f3134
Mann Jaiswal <mannjaiswal5@gmail.com>
2024-11-17 17:21:23 -0700

    Simple Node example

---
+++ app.js
@@ -0,0 +1,23 @@
+const rectangle = {
+    perimeter: (x,y) => 2 * (x + y),
+    area: (x, y) => x * y
+};
+
+function solveRect(length, width){
+console.log(`Solving for rectangle with dimensions: ${length}, ${width}`);
+
+if (length <= 0 || width <= 0){
+    console.log(`Rectangle dimensions must be greater than zero. Received dimensions: ${length}, ${width}`);
+
+}
+else {
+    console.log(`Perimeter of rectangle: ${rectangle.perimeter(length, width)}`);
+    console.log(`Area of rectangle: ${rectangle.area(length,width)}`);
+}
+};
+
+solveRect(0,0);
+solveRect(2,4);
+solveRect(0,5);
+solveRect(5, -3);
+solveRect(3,5);
